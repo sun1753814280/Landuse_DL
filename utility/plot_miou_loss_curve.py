2b00bae895e31a21503f29af89b4339511abf509
cryoyan <huyan@link.cuhk.edu.hk>
2021-03-26 15:11:04 +0800

    x-connector bug

--- utility/plot_miou_loss_curve.py
+++ utility/plot_miou_loss_curve.py
@@ -19,8 +19,8 @@ import basic_src.basic as basic
 
 import matplotlib
 # must be before importing matplotlib.pyplot or pylab!
-if os.name == 'posix' and "DISPLAY" not in os.environ:
-    matplotlib.use('Agg')
+# if os.name == 'posix' and "DISPLAY" not in os.environ:
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
